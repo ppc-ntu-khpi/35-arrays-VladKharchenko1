601157c10ba4e253449978eddc2f0c008b414732
VladKharchenko <72339081+VladKharchenko1@users.noreply.github.com>
2021-01-31 23:23:12 +0200

    Update Exercise.java

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,25 @@
 package domain;
-
-public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
-    }
+/**
+ * Клас для створення симетричного масиву
+ * @author Vlad
+ */
+public final class Exercise implements ExerciseDodatok {
+    private final int[] numbers;
+/**
+ * Присвоєння
+ * @param length величина масиву
+ */
+    public Exercise(final int length) {
+        this.numbers = new int[length];}
+/**
+ * Створення симетричного масиву
+ * @return numbers 
+ */
+    @Override
+    public int[] symmetricArray() {
+        final int length = this.numbers.length;
+        int count = (length % 2 == 0) ? 1 : 0;
+        for (int i = length / 2; i < length; i++) {
+            this.numbers[i] = this.numbers[length - i - 1] = count++; }
+        return this.numbers;}
 }
