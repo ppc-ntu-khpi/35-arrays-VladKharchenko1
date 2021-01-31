248bacc8d7fc0fbb6f33ba91df61e408f7b41d8a
VladKharchenko <72339081+VladKharchenko1@users.noreply.github.com>
2021-01-31 23:22:30 +0200

    Update TestResult.java

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,22 @@
 package test;
 
 import domain.Exercise;
-
+import domain.ExerciseDodatok;
+import java.util.Arrays;
+/**
+ * Клас для взаємодії програма-користовувач, програма-програміст
+ * @author Vlad
+ */
 public class TestResult {
-
+/**
+ * Тут можна задати величину масиву в строкі final int length = ***;
+ * Виводить результат
+ * @param args аргументи 
+ */
     public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        final int length = 15;
+        final ExerciseDodatok numberArray = new Exercise(length);
+        final int[] array = numberArray.symmetricArray();
+        System.out.println(Arrays.toString(array));
     }
 }
